f8213f7a7c4d5c8d9b673fd62d4a190444ee155e
Mark Silvis <marksilvis@pitt.edu>
2015-11-12 15:24:14 -0500

    add CCD DB application

--- src/main/java/edu/pitt/dbmi/ccd/security/CCDSecurityApplication.java
+++ src/main/java/edu/pitt/dbmi/ccd/security/CCDSecurityApplication.java
@@ -4,11 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.ApplicationContext;
+import edu.pitt.dbmi.ccd.db.CCDDatabaseApplication;
 
 /**
  * @author Mark Silvis (marksilvis@pitt.edu)
  */
 @SpringBootApplication
+@Import({CCDDatabaseApplication.class})
 public class CCDSecurityApplication {
 
     public static void main(String[] args) {
